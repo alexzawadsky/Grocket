cfbeda86be63d95cbe40c95310330cba2476e9ad
Timur <timurram007@gmail.com>
2023-02-17 18:12:05 +0300

    favourites

--- frontend/src/pages/MyFavourites.jsx
+++ frontend/src/pages/MyFavourites.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react'
-import { useLoaderData, NavLink } from 'react-router-dom'
-import ItemCard from '../components/ItemCard'
+import React, { useState, useEffect } from 'react'
+import { NavLink } from 'react-router-dom'
+import { ItemCard, Pagination, Title } from '../components'
 import { useMediaQuery } from 'react-responsive'
 import { BsArrowLeft } from 'react-icons/bs'
+import useAxios from '../hooks/useAxios'
+import { AiFillHeart } from 'react-icons/ai'
 
 
 const data = [
@@ -63,20 +65,33 @@ const data = [
 
 const MyFavourites = () => {
 
-    const [favourites, setFavourites] = useState()
-    const isTablet = useMediaQuery({ query: '(min-width: 768px)' })
+    const [products, setProducts] = useState()
+    const [page, setPage] = useState(0)
+    const [pagesCount, setPagesCount] = useState(1)
+    const isPhone = useMediaQuery({ query: '(max-width: 639px)' })
+    const api = useAxios()
+
+    const loadPage = () => {
+        api.get(`/api/v1/products/?limit=4&page=${page + 1}&is_favourited=1`).then(res => {
+            console.log(res)
+            setProducts(res.data.results)
+            setPagesCount(res.data.pages_count)
+        }).catch(err => alert(`${err.response.status} ${err.response.message}`))
+    }
 
     useEffect(_ => {
-        setFavourites(data)
-    }, [])
+        loadPage()
+    }, [page])
+
 
     return (
-        <div className='grid gap-5'>
-            {!isTablet ? <NavLink className='flex items-center gap-2' to='/profile'><BsArrowLeft />Back to profile</NavLink> : null}
-            <h1 className="font-bold text-3xl">My favourites</h1>
-            <div className='grid grid-cols-2 lg:grid-cols-4 gap-5'>
-                {favourites.map((el, key) => <ItemCard key={key} content={el} />)}
+        <div className='grid gap-5 w-full'>
+            {isPhone ? <NavLink className='flex items-center gap-2' to='/profile'><BsArrowLeft />Back to profile</NavLink> : null}
+            <div className='flex items-center gap-2'><p className='text-accent-red text-3xl'><AiFillHeart /></p><Title text='Favourites' /></div>
+            <div className='grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5'>
+                {products && products.map((el, key) => <ItemCard product={el} key={key} />)}
             </div>
+            <Pagination pagesCount={pagesCount} setPage={setProducts} />
         </div>
     )
 }

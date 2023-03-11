e97b7996aebc938a2195750048bce7d21907c2da
Timur Ramazanov <timurram007@gmail.com>
2023-03-12 00:23:01 +0100

    fix

--- frontend/src/pages/Sell/utils/index.js
+++ frontend/src/pages/Sell/utils/index.js
@@ -7,22 +7,29 @@ export const deleteImage = (imageIndex, images, setMainImageIndex, setImages) =>
     setImages(images.filter((el, key) => key !== imageIndex))
 }
 
-export const saveImage = async (editorRef, images, setImages, setCurrentImage, imageInputRef) => {
-    const img = new Image()
-    img.src = editorRef.current.getImage()
-    img.onload = () => {
-        const canvas = document.createElement("canvas")
-        canvas.width = 800
-        canvas.height = 600
-        const ctx = canvas.getContext("2d")
-        ctx.drawImage(img, 0, 0, 800, 600)
-        const newImage = {
-            is_main: images.length > 0 ? false : true,
-            image: canvas.toDataURL("image/png")
+export const saveImage = (editorRef, images, setImages, setCurrentImage, imageInputRef, setUploading) => {
+    try {
+        setUploading(true)
+        const img = new Image()
+        img.src = editorRef.current.getImage().toDataURL()
+        img.onload = () => {
+            const canvas = document.createElement("canvas")
+            canvas.width = 800
+            canvas.height = 600
+            const ctx = canvas.getContext("2d")
+            ctx.drawImage(img, 0, 0, 800, 600)
+            const newImage = {
+                is_main: images.length > 0 ? false : true,
+                image: canvas.toDataURL("image/png")
+            }
+            setImages([...images, newImage])
+            imageInputRef.current.value = null
+            setCurrentImage(null)
         }
-        setImages([...images, newImage])
-        imageInputRef.current.value = null
-        setCurrentImage(null)
+        setUploading(false)
+    } catch (e) {
+        setUploading(false)
+        alert(e)
     }
 }
 

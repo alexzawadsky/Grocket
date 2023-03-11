985c873851ac392576dc27c13a085ac9eaed3f3b
Timur Ramazanov <timurram007@gmail.com>
2023-03-11 23:10:08 +0100

    fix

--- frontend/src/pages/Landing/HistoryItem.jsx
+++ frontend/src/pages/Landing/HistoryItem.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom"
 const HistoryItem = ({ content }) => {
     return (
         <NavLink to={`/products/${content.id}`} className='border-2 border-black rounded-lg p-5 flex gap-5 items-center shadow-lg w-full'>
-            <img src={content.images.filter(el => el.is_main)[0].image} className='w-1/4 rounded-lg' />
+            <img src={content?.images.filter(el => el?.is_main)[0]?.image} className='w-1/4 rounded-lg' />
             <div>
                 <p className='text-sm xl:text-lg hover:text-accent-orange overflow-hidden truncate'>{content.name}</p>
                 <p className='font-bold'>{parseFloat(content.price).toFixed(0)} {content.price_currency}</p>
--- frontend/src/pages/Sell/utils/index.js
+++ frontend/src/pages/Sell/utils/index.js
@@ -8,17 +8,22 @@ export const deleteImage = (imageIndex, images, setMainImageIndex, setImages) =>
 }
 
 export const saveImage = async (editorRef, images, setImages, setCurrentImage, imageInputRef) => {
-    const newImage = {
-        is_main: images.length > 0 ? false : true,
-        image: editorRef.current.getImage().toDataURL()
-    }
-    if (images) {
+    const img = new Image()
+    img.src = editorRef.current.getImage()
+    img.onload = () => {
+        const canvas = document.createElement("canvas")
+        canvas.width = 800
+        canvas.height = 600
+        const ctx = canvas.getContext("2d")
+        ctx.drawImage(img, 0, 0, 800, 600)
+        const newImage = {
+            is_main: images.length > 0 ? false : true,
+            image: canvas.toDataURL("image/png")
+        }
         setImages([...images, newImage])
-    } else {
-        setImages([newImage])
+        imageInputRef.current.value = null
+        setCurrentImage(null)
     }
-    imageInputRef.current.value = null
-    setCurrentImage(null)
 }
 
 export const prepareImages = (images) => {

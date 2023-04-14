a0ffd571d6ca28fff3d615251cc00adfa0654ebd
Timur Ramazanov <timurram007@gmail.com>
2023-04-14 12:28:18 +0200

    cat link, removed heart from my product page

--- frontend/src/pages/Product/Category.jsx
+++ frontend/src/pages/Product/Category.jsx
@@ -1,15 +1,26 @@
 import { BsArrowRight } from "react-icons/bs"
+import { NavLink } from "react-router-dom"
 
 const Category = ({ category }) => {
     return (
         <ul className="flex gap-3 md:gap-5 flex-wrap items-center" aria-label='list of product categories'>
-            {category.parents.map((el, key) => (
-                <li className="flex gap-3 md:gap-5 items-center" key={key}>
-                    {el.title}<BsArrowRight />
-                </li>
-            ))}
-            <li className='font-bold'>{category.title}</li>
-        </ul>
+            {category.parents.map((el, key) => <li key={key} className="flex gap-3 md:gap-5 items-center">
+                <NavLink
+                    to={`/search?category_id=${el?.id}`}
+                    className='hover:text-accent-orange'
+                >
+                    {el.title}
+                </NavLink>
+                <BsArrowRight />
+            </li>)}
+            <li>
+                <NavLink
+                    to={`/search?category_id=${category?.id}`}
+                    className='font-bold hover:text-accent-orange'
+                >
+                    {category.title}
+                </NavLink></li>
+        </ul >
     )
 }
 

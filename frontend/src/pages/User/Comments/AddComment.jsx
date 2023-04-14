4bf5a2bd2ce9e668e93ab2fca98d2422f00100fd
Timur Ramazanov <timurram007@gmail.com>
2023-04-14 19:20:01 +0200

    add comments darkmode

--- frontend/src/pages/User/Comments/AddComment.jsx
+++ frontend/src/pages/User/Comments/AddComment.jsx
@@ -1,7 +1,7 @@
 import { NavLink, useParams } from "react-router-dom"
 import { BsArrowLeft } from "react-icons/bs"
 import { useTranslation } from "react-i18next"
-import { Price, Title, Spinner, Input, ImageEditor, RatingStars } from "../../../components/ui"
+import { Title, Spinner, Input, ImageEditor, RatingStars, Button, Form } from "../../../components/ui"
 import { useEffect, useState } from "react"
 import { useCommentStatuses, useUploadComment, useUserProducts } from "../../../api/api"
 import CommentSelectItem from "./CommentSelectItem"
@@ -18,14 +18,12 @@ const AddComment = () => {
     const statuses = useCommentStatuses()
     const [filteredProducts, setFilteredProducts] = useState(products?.data?.results)
     const [selectedProduct, setSelectedProduct] = useState(null)
-    const [filterString, setFilterString] = useState('')
+    const filterString = useInput('')
     const [statusId, setStatusId] = useState(null)
     const [images, setImages] = useState([])
     const text = useInput('', {})
     const [rating, setRating] = useState(1)
     const addCommentMutation = useUploadComment()
-
-    const setMainIndex = () => { }
 
     useEffect(() => {
         setFilteredProducts(products?.data?.results)
@@ -34,19 +32,18 @@ const AddComment = () => {
     useEffect(() => {
         setFilteredProducts(
             products?.data?.results.filter(el =>
-                el.name.toLowerCase().includes(filterString.toLocaleLowerCase()))
+                el.name.toLowerCase().includes(filterString.value.toLocaleLowerCase()))
         )
-    }, [filterString])
+    }, [filterString.value])
 
     const handleUpload = () => {
-        const data = {
+        addCommentMutation.mutate({
             product: selectedProduct.id,
             rate: rating,
             status: statusId,
             images: images,
             text: text.value
-        }
-        addCommentMutation.mutate(data)
+        })
     }
 
     return (
@@ -55,64 +52,63 @@ const AddComment = () => {
                 <BsArrowLeft />{t('back_to_comments')}
             </NavLink>
             <Title text={t('add_comment')} />
-            <div className="grid md:grid-cols-[1fr_2fr] gap-5">
+            <Form className="grid md:grid-cols-[1fr_2fr] gap-5" onSubmit={handleUpload}>
                 <h2 className="font-bold text-xl">{t('select_product')}</h2>
                 <div className="grid gap-3">
-                    {!selectedProduct &&
-                        <>
-                            <input
-                                className="grocket-input w-full md:w-2/3 lg:w-1/2"
-                                placeholder={t('type_to_filter')}
-                                onChange={(e) => setFilterString(e.target.value)}
-                            />
-                            {products.isLoading && <Spinner />}
-                            {products.error && error.message}
-                            <div className="flex flex-wrap gap-3">
-                                {
-                                    filteredProducts?.map((el, key) =>
-                                        <CommentSelectItem
-                                            key={key}
-                                            item={el}
-                                            onClick={() => setSelectedProduct(el)}
-                                            selectable
-                                        />)
-                                }
-                            </div>
-                        </>
+                    {!selectedProduct && <>
+                        <Input
+                            instance={filterString}
+                            className="w-full md:w-2/3 lg:w-1/2"
+                            placeholder={t('type_to_filter')}
+                            onChange={(e) => setFilterString(e.target.value)}
+                        />
+                        {products.isLoading && <Spinner />}
+                        {products.error && error.message}
+                        <ul className="flex flex-wrap gap-3">
+                            {
+                                filteredProducts?.map((el, key) => <li key={key}>
+                                    <CommentSelectItem
+                                        item={el}
+                                        onClick={() => setSelectedProduct(el)}
+                                        selectable
+                                    />
+                                </li>)
+                            }
+                        </ul>
+                    </>
                     }
                     {selectedProduct &&
                         <div className="flex items-center gap-5">
                             <CommentSelectItem item={selectedProduct} />
-                            <button
+                            <Button
+                                type='button'
                                 onClick={() => setSelectedProduct(null)}
                                 className='text-accent-red'
+                                border={false}
                             >
                                 <BsTrashFill />
-                            </button>
-                        </div>
-                    }
+                            </Button>
+                        </div>}
                 </div>
-                {
-                    selectedProduct &&
-                    <>
-                        <h2 className="font-bold text-xl">{t('status')}</h2>
-                        <div className="grid gap-1 p-1 rounded-xl border shadow-sm   grid-cols-[1fr_1fr] w-fit">
-                            {
-                                statuses.data && statuses.data?.map((el, key) =>
-                                    <div
-                                        className={`h-10 rounded-lg hover:bg-slate-100 px-2 flex items-center font-bold cursor-pointer gap-2 ${el?.id === statusId && '!bg-slate-200'}`}
-                                        key={key}
-                                        onClick={() => setStatusId(el?.id)}
-                                    >
-                                        <CommentStatus
-                                            title={el?.title}
-                                            name={el?.name}
-                                        />
-                                    </div>
-                                )
-                            }
-                        </div>
-                    </>
+                {selectedProduct && <>
+                    <h2 className="font-bold text-xl">{t('status')}</h2>
+                    <ul className="grid gap-1 p-1 rounded-xl border dark:border-2 dark:border-zinc-600 shadow-sm   grid-cols-[1fr_1fr] w-fit">
+                        {statuses.data && statuses.data?.map((el, key) => <li key={key}>
+                            <Button
+                                type='button'
+                                className={`h-10 rounded-lg hover:bg-slate-100 dark:hover:bg-zinc-700 px-2 flex items-center font-bold cursor-pointer gap-2 ${el?.id === statusId && '!bg-slate-200 dark:!bg-zinc-700'} !justify-start !rounded-lg`}
+                                onClick={() => setStatusId(el?.id)}
+                                width='full'
+                                border={false}
+                            >
+                                <CommentStatus
+                                    title={el?.title}
+                                    name={el?.name}
+                                />
+                            </Button>
+                        </li>)}
+                    </ul>
+                </>
                 }
                 {
                     statusId && selectedProduct &&
@@ -134,9 +130,9 @@ const AddComment = () => {
                         <div className="flex flex-col gap-3">
                             <p className="font-bold text-lg">{t('list_of_comment_images')}</p>
                             <p className="text-green-500">{t('images_are_optional')}</p>
-                            <div className='col-span-2 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 h-fit'>
+                            <ul className='col-span-2 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 h-fit'>
                                 {images.map((el, key) =>
-                                    <div key={key} className='w-full p-2 rounded-lg border-2 grid gap-2'>
+                                    <li key={key} className='w-full p-2 rounded-lg border-2 grid gap-2'>
                                         <img className='w-full' src={el?.image} />
                                         <button
                                             type='button'
@@ -144,23 +140,27 @@ const AddComment = () => {
                                             className='text-accent-red'>
                                             <BsTrashFill />
                                         </button>
-                                    </div>)}
-                            </div>
+                                    </li>)}
+                            </ul>
                         </div>
                         <div className="col-span-full">
                             <h2 className="font-bold text-xl mb-1">{t('rating')}</h2>
                             <RatingStars rating={rating} setRating={setRating} />
                         </div>
-                        <button
-                            className="button-fill-orange"
-                            onClick={handleUpload}
+                        <Button
+                            style='fill'
+                            color='accent-orange'
+                            type='submit'
+                            height={12}
+                            width='fit'
+                            px={5}
                             disabled={text.value === ''}
                         >
                             {t('upload')}
-                        </button>
+                        </Button>
                     </>
                 }
-            </div>
+            </Form>
         </div>
     )
 }

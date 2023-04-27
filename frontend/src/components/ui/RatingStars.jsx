7872978176037d924e0ab41b80a60a65d4fb27cc
Timur Ramazanov <timurram007@gmail.com>
2023-04-27 18:04:53 +0200

    rating stars fix

--- frontend/src/components/ui/RatingStars.jsx
+++ frontend/src/components/ui/RatingStars.jsx
@@ -7,26 +7,30 @@ const RatingStars = ({ rating, setRating }) => {
     return (
         <ul
             className='flex gap-1 w-fit'
-            onMouseLeave={() => setLocalRating(rating)}
+            onMouseLeave={() => setRating && setLocalRating(rating)}
             aria-label={`rating stars ${rating} out of 5`}
         >
             {Array(5).fill(0).map((_, i) => (
                 <li
                     key={i}
-                    style={{
+                    style={setRating ? {
                         color: i + 1 <= localRating + 0.3 ?
                             '#FF9001'
-                            : i + 1 <= rating ? '#ffd8a6' : '#C0D6E4',
+                            : i + 1 <= rating ? '#ffd8a6' : '#C0D6E4'
+                    } : {
+                        color: i + 1 <= rating + 0.3 ?
+                            '#FF9001' : '#C0D6E4'
                     }}
-                    className={setRating && 'cursor-pointer text-3xl'}
+                    className={setRating ? 'cursor-pointer text-3xl' : ''}
                     onMouseEnter={() => setRating ? setLocalRating(i + 1) : null}
                     onClick={() => setRating(i + 1)}
                     aria-hidden='true'
                 >
                     &#9733;
                 </li>
-            ))}
-        </ul>
+            ))
+            }
+        </ul >
     )
 }
 

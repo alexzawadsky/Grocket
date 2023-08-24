c6a4dd66ffe519cca863b509137ba03504766667
Timur Ramazanov <timurram007@gmail.com>
2023-08-24 23:44:27 +0200

    notifications

--- frontend/src/handlers/ws.js
+++ frontend/src/handlers/ws.js
@@ -1,3 +1,31 @@
+const blinkTabWhenInBackground = () => {
+    const originalTitle = document.title
+    let isTabBlinking = false;
+
+    const blinkInterval = setInterval(() => {
+        if (!document.hidden) {
+            clearInterval(blinkInterval)
+            document.title = originalTitle
+        } else {
+            if (!isTabBlinking) {
+                document.title = "New Message!"
+            } else {
+                document.title = originalTitle
+            }
+            isTabBlinking = !isTabBlinking
+        }
+    }, 1000)
+}
+
+const openPage = (url) => {
+    const existingTab = Array.from(window.frames).find(frame => frame.location.href === url)
+    if (existingTab) {
+        existingTab.focus()
+    } else {
+        window.open(url, "_blank")
+    }
+}
+
 export const addNewMessage = (message, queryClient) => {
     const currentData = queryClient.getQueryData([
         'messenger',
@@ -21,4 +49,17 @@ export const addNewMessage = (message, queryClient) => {
             pageParams: currentData.pageParams,
         }
     )
+    Notification.requestPermission().then(perm => {
+        if (perm === 'granted' && document.hidden) {
+            const notification = new Notification("New Message", {
+                body: message?.text,
+                icon: "logo.png"
+            })
+            notification.onclick = () => {
+                openPage(`${import.meta.env.VITE_API_URL}/messenger/${message?.chat}`)
+            }
+            blinkTabWhenInBackground()
+        }
+    })
+
 }
